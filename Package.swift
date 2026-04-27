504ce3e93e66df5010c41928655c6e228eab0fb5
RoadSave iOS SDK CI <ios-sdk-ci@dynamus-technologies.com>
2026-04-27 10:37:56 +0000

    chore: bump binaryTarget to 4.0.0-rc.1

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .binaryTarget(
             name: "RoadSaveKit",
             url: "https://github.com/Dynamus-Technologies/roadsave-ios-sdk/releases/download/4.0.0/RoadSaveKit-4.0.0.zip",
-            checksum: "placeholder-updated-on-first-release"
+            checksum: "56bd8b919361514293a86781a114b4eb9bb9320845e8ce00ac5d1a7eb97fa490"
         )
     ]
 )
